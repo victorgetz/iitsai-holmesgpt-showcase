17847b70b268acb726cd5b3118fe40b92a7b21c4
Victor Getz <sd>
2024-06-26 16:47:56 +0200

    init

---
+++ files/holmes.sh
@@ -0,0 +1,3 @@
+export OPENAI_API_KEY="REPLACE_ME"
+export OPENAI_BASE_URL="https://dev.ai.iits.tech/backend/holmes"
+#holmes ask --model holmes_rag_pipeline "Why does me ingress my-awesome-ingress from the namespace test not have a public ip?"
